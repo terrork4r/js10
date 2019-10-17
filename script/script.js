9a258852d7e757daed581d6ae53d78295287782b
terrork4r <52910009+terrork4r@users.noreply.github.com>
2019-10-18 01:01:57 +0300

    сделал первый урок, создал ветку leson01, создал переменные вывел в консоль сообщение и вывел модальное окно alert

--- script/script.js
+++ script/script.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+'use strick';
+
+let money,
+    income,
+    addExpenses,
+    deposit,
+    mission,
+    period;
+    
+alert('hello world');
+console.log('Ну вот и начались бессоные ночи');
