747debd1afcf4b6c2c8c94d7d56409cf64f8022c
terrork4r <52910009+terrork4r@users.noreply.github.com>
2019-10-22 02:21:06 +0300

    Сделал дополнительное задание

--- dopDz/script.js
+++ dopDz/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+let lang = prompt('Введите удобный для вас язык в формате ru или en', 'ru');
+const ru = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
+      en = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
+      error = 'Что то пошло не так, попробуйте перезагрузите страницу и убедитесь в правильности ввода языка';
+
+if(lang === 'ru'){
+  alert(ru);
+}else if(lang === 'en'){
+  alert(en);
+}else{
+  alert(error);
+}
+
+
+switch(lang){
+  case 'ru': 
+    alert(ru);
+  break;
+  case 'en':
+    alert(en);
+    break;
+  default:
+      alert(error);   
+}
+
+const arr = [ru, en];
+
+let result = lang === 'ru' ? alert(arr[0]) : lang === 'en' ? alert(arr[1]) : alert(error);
+
+
+
+const namePerson = prompt('Введите ваше имя', 'Влад');
+
+let result2 = namePerson === 'Артём' ? console.log('Директор') : 
+              namePerson === 'Максим' ? console.log('Преподаватель') :
+              console.log('Студент');
