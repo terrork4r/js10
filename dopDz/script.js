02b8d3b3efe6e5b0fbfd11de590cc4ae409e91ba
terrork4r <52910009+terrork4r@users.noreply.github.com>
2019-10-22 02:22:36 +0300

    перезалил доп задание 3 урока

--- dopDz/script.js
+++ dopDz/script.js
@@ -5,6 +5,7 @@ const ru = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
       en = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
       error = 'Что то пошло не так, попробуйте перезагрузите страницу и убедитесь в правильности ввода языка';
 
+// Через if
 if(lang === 'ru'){
   alert(ru);
 }else if(lang === 'en'){
@@ -13,7 +14,7 @@ if(lang === 'ru'){
   alert(error);
 }
 
-
+//Через switch
 switch(lang){
   case 'ru': 
     alert(ru);
@@ -25,11 +26,12 @@ switch(lang){
       alert(error);   
 }
 
+// С помошью многомерного массива и тернарных операторов
 const arr = [ru, en];
 
 let result = lang === 'ru' ? alert(arr[0]) : lang === 'en' ? alert(arr[1]) : alert(error);
 
-
+//Второе задание
 
 const namePerson = prompt('Введите ваше имя', 'Влад');
 
