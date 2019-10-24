d6fdb1e099497230df3e5c657491a4bd3c479586
terrork4r <52910009+terrork4r@users.noreply.github.com>
2019-10-24 03:31:05 +0300

    сделал усложненное

--- dopDz/script.js
+++ dopDz/script.js
@@ -1,7 +1,18 @@
 'use stict';
 
-const math = function(a, b){
-  return a * b;
+// Создайте функцию, которая принимает 1 аргумент (название произвольное)
+// — Если как аргумент передана не строка - функция оповещает об этом пользователя
+// — В полученной (как аргумент) строке функция должна убрать все пробелы в начале и в конце
+// — Если строка более 30 знаков - то после 30го символа часть текста скрывается и вместо них появляются три точки (...)
+const lineHandler = function(text){
+    if(typeof text !== 'string'){
+      return console.log('Проверьте правильность ввода, что то пошло не так');
+    }else if(text.length > 30) {
+      return console.log(text.trim().slice(0, 29) + '...');
+    } else if(typeof text == 'string') {
+      return console.log(text.trim());
+     }
+    
 };
-let result = math(10, 30);
-console.log(result);
+
+lineHandler('Приветттттттттттттттттттттттттттттттттттттттттттттттттт');
