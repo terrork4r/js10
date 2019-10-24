7e0ee025fd1b51346259bdeabbff44b5cbb656f7
terrork4r <52910009+terrork4r@users.noreply.github.com>
2019-10-24 03:41:46 +0300

    внес некоторые поправки в усложненное

--- dopDz/script.js
+++ dopDz/script.js
@@ -4,15 +4,15 @@
 // — Если как аргумент передана не строка - функция оповещает об этом пользователя
 // — В полученной (как аргумент) строке функция должна убрать все пробелы в начале и в конце
 // — Если строка более 30 знаков - то после 30го символа часть текста скрывается и вместо них появляются три точки (...)
-const lineHandler = function(text){
-    if(typeof text !== 'string'){
-      return console.log('Проверьте правильность ввода, что то пошло не так');
-    }else if(text.length > 30) {
+const lineHandler = function (text) {
+    if (typeof text !== 'string') {
+      return console.log ('Проверьте правильность ввода, что то пошло не так') ;
+    }else if (text.length > 30) {
       return console.log(text.trim().slice(0, 29) + '...');
-    } else if(typeof text == 'string') {
-      return console.log(text.trim());
+    } else if (typeof text == 'string') {
+      return console.log (text.trim()) ;
      }
     
 };
 
-lineHandler('Приветттттттттттттттттттттттттттттттттттттттттттттттттт');
+lineHandler ('Приветттттттттттттттттттттттттттттттттттттттттттттттттт');
