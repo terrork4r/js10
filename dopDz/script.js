cfd275f2f2f4e48bcc609943db6865364f2abba1
terrork4r <52910009+terrork4r@users.noreply.github.com>
2019-10-30 22:33:03 +0300

    сделал усложненное 6 урока

--- dopDz/script.js
+++ dopDz/script.js
@@ -1,13 +1,22 @@
 'use stict';
-let car = {
-  collor: 'red',
-  speed: 200,
-  model: 'lada',
-  year: 2010,
-};
-for( let key in car){
-  console.log(key);
+// 1) Создать массив week и записать в него дни недели в виде строк
+// ·        Вывести на экран все дни недели
+// ·        Каждый из них с новой строчки
+// ·        Выходные дни - курсивом
+// ·        Текущий день - жирным шрифтом(использовать объект даты)
+let data = new Date();
+let week = ['воскресенье', 'понедельник', 'вторние', 'среда', 'четверг', 'пятница', 'суббота'];
+for(let i =0; i<7; i++){
+  
+  if(week[i] === 'суббота' || week[i] === 'воскресенье'){
+    console.log('%c' + week[i], "font-style: italic;");
+  }
+  if(week[i] === week[data.getDay()]){
+    console.log('%c' + week[i], "font-weight: bold;");
+  }
+  if(week[i] === 'суббота' || week[i] === 'воскресенье'|| week[i] === week[data.getDay()]){
+   continue;
+  }
+  console.log(week[i]);
 }
 
-let arr = [1, 2, 4, 3, 5];
-console.log(arr.join());
