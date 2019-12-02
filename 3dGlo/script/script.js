818fe5726b70098fd87d1ffe1dec9e985a5dc1e5
terrork4r <52910009+terrork4r@users.noreply.github.com>
2019-12-02 21:14:17 +0300

    сделал таймер

---
+++ 3dGlo/script/script.js
@@ -0,0 +1,56 @@
+
+window.addEventListener('DOMContentLoaded', function() {
+  'use strict';
+  const qs = document.querySelector.bind(document);
+  const qsAll = document.querySelectorAll.bind(document);
+  // Timer
+  function countTimer(deadline) {
+    let timerHours = qs('#timer-hours'),
+    timerMinutes = qs('#timer-minutes'),
+    timerSecond = qs('#timer-seconds');
+
+    function getTimeReamaining() {
+      let dateStop = new Date(deadline).getTime(),
+          dateNow = new Date().getTime(),
+          timeRemaining = (dateStop - dateNow) / 1000,
+          seconds = Math.floor(timeRemaining % 60).toString(),
+          minutes = Math.floor((timeRemaining / 60) % 60).toString(),
+          hours = Math.floor(timeRemaining / 60 / 60).toString();
+          if(hours.length < 2) {
+            hours = '0' +  hours;
+          }
+          if(minutes.length < 2) {
+            minutes = '0' +  minutes;
+          }
+          if(seconds.length < 2){
+            seconds = '0' + seconds;
+          }
+          
+          return {timeRemaining, hours, minutes, seconds};
+          
+    }
+    function updateCLock() {
+      let timer = getTimeReamaining();
+      
+        timerHours.textContent = timer.hours;
+        timerMinutes.textContent = timer.minutes;
+        timerSecond.textContent = timer.seconds;
+        
+        if(timer.timeRemaining > 0) {
+          setInterval(updateCLock, 1000);
+        }else {
+          timerHours.textContent = '00';
+          timerMinutes.textContent = '00';
+          timerSecond.textContent = '00';
+          clearInterval(updateCLock);
+        }    
+    } 
+
+    updateCLock();
+  }
+  
+  countTimer('3 dec 2019');
+
+
+
+});
--- dopDz/script.js
+++ dopDz/script.js
@@ -1,13 +1,36 @@
 'use strict';
-class Car {
-  constructor() {
-    this.brand = 'mazda';
-    this._model = 3;
+const qs = document.querySelector.bind(document);
+const qsAll = document.querySelectorAll.bind(document);
+
+
+const box1 = qs('.box1'),
+      box2 = qs('.box2');
+let interval;
+let count = 0;
+let animate = function() {
+  interval = requestAnimationFrame(animate);    
+    count++;
+    if(count < 500){
+    box1.style.top = count + 'px';
+    box1.style.left = count / 2 + 'px';
+    box2.style.left = count * 2 + 'px';
+    }else {
+      count = 0;
+    }
+};
+let a = false;
+document.addEventListener('click', () => {
+  if(a) {
+  interval = requestAnimationFrame(animate); 
+  a = false;
+  }else {
+    a = true;
+    cancelAnimationFrame(interval);
   }
-  fun() {
-    console.log(this.brand + ' ' + this.model);
-    Car.yes++;
-  }
-}
-const car = new Car();
-console.log(Car._model);
+});
+
+let date = new Date(1995, 6, 7, 23, 25, 30, 200);
+console.log("TCL: date", date);
+
+
+
