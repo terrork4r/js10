17439f591f5283c9da0fa88310ad77407037acd8
terrork4r <52910009+terrork4r@users.noreply.github.com>
2019-12-03 17:28:50 +0300

    сделал таймер в новом файле, вторая часть домашнего задания

--- bonusDz/script.js
+++ bonusDz/script.js
@@ -1,55 +1,84 @@
-'use strict';
-// let a = document.querySelector('#a'),
-//     b = document.querySelector('#b'),
-//     sum = document.querySelector('#sum'),
-//     mult = document.querySelector('#mult'),
-//     res = document.querySelector('#res');
+window.addEventListener('DOMContentLoaded', function() {
+  'use strict';
+  const qs = document.querySelector.bind(document),
+        qsAll = document.querySelectorAll.bind(document);
+
+  let welcome = qs('#welcome'),
+      weekDay = qs('#weekDay');
+      
+  let timerHours = qs('#timer-hours'),
+      timerMinutes = qs('#timer-minutes'),
+      timerSeconds = qs('#timer-seconds');
+
+  let daysNewYear = qs('#daysNewYear'),
+      dateNewYear = new Date(2020, 0, 1);
+  
+  function upgrade() {
+    let dateNow = new Date(),
+      weekDay = dateNow.getDay().toString(),
+      hours = dateNow.getHours().toString(),
+      minutes = dateNow.getMinutes().toString(),
+      seconds = dateNow.getSeconds().toString(),
+      getTimeNow = dateNow.getTime();
     
-// const calculator = {
-//   result: 0,
-//   sum: function(){
-//     this.result = Number(a.value) + Number(b.value);
-//     this.show();
-//   },
-//   mult: function(){
-//     this.result = a.value * b.value;
-//     this.show(); 
-//   },
-//   show: function(){
-//     res.value = this.result;
-//   }
-// };
+      if(hours.length < 2) {
+        hours = '0' +  hours;
+      }
+      if(minutes.length < 2) {
+        minutes = '0' +  minutes;
+      }
+      if(seconds.length < 2){
+        seconds = '0' + seconds;
+      }
+      return {weekDay, hours, minutes, seconds, getTimeNow};
+  }   
 
-// sum.addEventListener('click', calculator.sum.bind(calculator));
-// mult.addEventListener('click', calculator.mult.bind(calculator));
-
-// Второе бонус задание
-//  let showResult = function(a, b){
-//    let res = a ** b;
-//    res = String(res).split("");
-//    console.log(res);
-//    const reducer = (accumulator, currentValue) => +accumulator + +currentValue;
-//    res = res.reduce(reducer);
-//    console.log(res);
-//  };
-//  showResult(5, 10);
-
-
-
-let arr = {
-  a: 10,
-  b: 12,
-  c: {
-    c1: () => {
-      console.log(this);
+  function welcomes() {
+    let welcomesUpgrade = upgrade();
+    if(welcomesUpgrade.hours < 12 && welcomesUpgrade.hours > 6) {
+      welcome.textContent = 'Доброе утро';
+    }else if(welcomesUpgrade.hours > 12 && welcomesUpgrade.hours < 18) {
+      welcome.textContent = 'Добрый день';
+    }else{
+      welcome.textContent = 'Доброй ночи';
     }
-  },
-  d: function() {
-    console.log(this);
-  },
-  i: [1, 2, 3, 4],
-};
-arr.i.forEach(function(item) {
-  console.log(this.a);
-});
+    setTimeout(welcomes, 360000);
+  }
+  function weekDays() {
+    let timeUpgrade = upgrade();
+    if(timeUpgrade.weekDay == 0){
+      weekDay.textContent = 'воскресенье';
+    }else if(timeUpgrade.weekDay == 1){
+      weekDay.textContent = 'понедельник';
+    } else if(timeUpgrade.weekDay == 2){
+      weekDay.textContent = 'вторник';
+    } else if(timeUpgrade.weekDay == 3){
+      weekDay.textContent = 'среда';
+    } else if(timeUpgrade.weekDay == 4){
+      weekDay.textContent = 'четверг';
+    } else if(timeUpgrade.weekDay == 5){
+      weekDay.textContent = 'пятница';
+    } else if(timeUpgrade.weekDay == 6){
+      weekDay.textContent = 'суббота';
+    }
+    setTimeout(weekDays, 360000);
+  }
 
+  function time() {
+    let timeUpgrade = upgrade();
+    timerHours.textContent = timeUpgrade.hours;
+    timerMinutes.textContent = timeUpgrade.minutes;
+    timerSeconds.textContent = timeUpgrade.seconds;
+  }
+
+  function newYear() {
+    let daysUpgrade = upgrade();
+    dateNewYear = dateNewYear.getTime();
+    daysNewYear.textContent = Math.floor((dateNewYear - daysUpgrade.getTimeNow) / 1000 / 60 / 60 / 24);
+    setTimeout(newYear, 360000);
+  }
+  welcomes();
+  weekDays();
+  setInterval(time, 1000);
+  newYear();
+});
